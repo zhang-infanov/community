a0c4d15dcf57af31996de53b145e762e98719264
zhang <zhang_infanov@foxmail.com>
2020-04-25 18:33:02 +0800

    add index and bootstrap

--- src/main/java/com/kaixin/community/controller/IndexController.java
+++ src/main/java/com/kaixin/community/controller/IndexController.java
@@ -11,10 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @create 2020-04-25-15:55
  */
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
